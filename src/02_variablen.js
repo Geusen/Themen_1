44667132d4c56751d48033cf9901072d065fbb0a
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-09 10:48:08 +0000

    Stand 09.01

---
+++ src/02_variablen.js
@@ -0,0 +1,26 @@
+// Zeilenkommentar
+/*
+    Block-Kommentar
+
+*/
+
+
+// console.log("Hi!"); // Zeilenkommentar hinter einem Befehl
+
+/***** Variablen 01 *******/
+
+// console.log("Hi!"); // Ausgabe
+// console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; //Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max";      // Wertzuweisung | assigment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin "+ firstName +" "+ familyName); // Ausgabe
+
+
+
+
