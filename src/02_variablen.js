4625c579cbf25ff75d865544b65afb0b181fcc29
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-09 11:25:35 +0000

    Stand 09.01. Nummer 2

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -14,13 +14,31 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; //Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max";      // Wertzuweisung | assigment
-console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin "+ firstName +" "+ familyName); // Ausgabe
+// let firstName; //Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Max";      // Wertzuweisung | assigment
+// console.log(firstName); // Ausgabe
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log("Hi, ich bin "+ firstName +" "+ familyName); // Ausgabe
 
+/*
+    Wertzuweisung 2 + Datentypen
+    JS ist eine untypisierte Sprache!
+*/
 
-
+let test; //    Deklaration
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------");
+test = "hello";                             // string
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------");
+test = 2;                                   // number
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------");
+test = true;                                // boolsche Zahl
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);             
 
