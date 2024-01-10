5433994a66cc8de3821637a1af93b95957864a32
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-10 11:33:12 +0000

    update

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,16 @@
+
+/* Berechnung Alter */
+
+// Deklaration
+let ageJohn, ageMark;
+const birthYearJohn = 2000;
+const birthYearMark = 1999;
+
+
+// Berechnung Alter
+ageJohn = 2024 - birthYearJohn;
+ageMark = 2024 - birthYearMark;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
