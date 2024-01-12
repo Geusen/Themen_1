3899c46fb855a26bcfb9af4cf3b4a88a40e78f0f
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-12 09:59:02 +0000

    update

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -4,13 +4,41 @@
 // 1. Kapselung von Codeblöcken
 
 // Funktionsaufruf | call
-test();
+// test();
 
 
-// Funktionsrumpf 
-// Funktionsdeklaration
-function test()
-{
-    console.log("Hallo, Benjamin");
+// // Funktionsrumpf 
+// // Funktionsdeklaration
+// function test()
+// {
+//     console.log("Hallo, Benjamin");
+// }
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+//ausgabeNamen();
+
+function ausgabeNamen() {
+    // interne Variable | What happens in functions stays at function
+    let firstName = "Maria";
+    console.log("Hallo, " + firstName +"!");
+    
 }
 
+
+//console.log(firstname); // Fehler : scope (Geltungsbereich)
+
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+ausgabeNamen2("Maria");    //Argument
+ausgabeNamen2("Nadim");
+ausgabeNamen2("Melanie"); 
+
+
+function ausgabeNamen2(firstName) {
+    
+    
+    console.log("Hallo, " + firstName +"!");
+}
