aaa417b37ec319a77b319e75a3788a8cd31082c5
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-15 09:44:58 +0000

    update

--- src/09_funktione_II.js
+++ src/09_funktione_II.js
@@ -30,6 +30,13 @@
 // output(multiply(3,-2));
 // output(multiply(3,0));
 
+// module: division a / b |  test:
+
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
 
 /******	3. Fkt. Grundrechenarten	*****/
 
@@ -48,9 +55,15 @@ function multiply(a,b) {
 	return a * b;
 }
 
-function multiply(a,b) {
+function divide(a,b) {
+	if (b !== 0) {
+		
 	
-	return a * b;
+	return a / b;
+	}
+	else{
+		return "Division durch Null ist nicht definiert";
+	}
 }
 
 // module: output | test:
