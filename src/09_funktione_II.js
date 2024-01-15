1520b968b170f2c162fc397b6fe4911775c84f28
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-15 11:44:22 +0000

    update

--- src/09_funktione_II.js
+++ src/09_funktione_II.js
@@ -10,48 +10,54 @@
 3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
+const prompt = require('prompt-sync')({sigint: true});
 
 const ERROR_STR_DIV = "Division durch 0 ist nicht möglich!";
 const ERROR_STR_GEN = "Irgendwas ging schief!";
 
+
+startApp();
+function startApp() {
+	output(calculator(getNum1(), getNum2(), getOp()));
+}
+
+function getNum1() {
+	return parseInt(prompt("Zahl1?: "));
+}
+
+function getNum2() {
+	return parseInt(prompt("Zahl2?: "));
+}
+
+function getOp() {
+	return prompt("OP?: ");
+}
+
 // module: calculator | tests:
 // agreement : "+","-","*",":","/"
 
-output(calculator(3,2,"+"));
-output(calculator(3,2,"-"));
-output(calculator(3,2,"*"));
-output(calculator(3,2,":"));
-output(calculator(3,2,"/"));
-output(calculator(3,0,"/"));
-output(calculator(3,2,"#?!"));
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
 
 function calculator(a, b, op) {
-switch (op) {
-	case "*": // add()
-		return add(a, b);
-	
+	switch (op) {
+		case "+": // add()
+			return add(a, b);
 		case "-": // sub()
-		
-		return subtract(a, b);
-
+			return subtract(a, b);
 		case "*": // mul()
-		
-		return multiply(a, b);
-
+			return multiply(a, b);
 		case ":": // div()
 		case "/": // div()
-		
-		return divide(a, b);
-
-	
-
-
-
-	default: // ERROR
-		return ERROR_STR_GEN;
-}
-	return "hi";
-	
+			return divide(a, b);
+		default: // ERROR
+			return ERROR_STR_GEN;
+	}
 }
 
 // module: addition a + b |  test:
@@ -85,22 +91,18 @@ switch (op) {
 /******	3. Fkt. Grundrechenarten	*****/
 
 function add(a,b) {
-
 	return a + b;
 }
 
 function subtract(a,b) {
-	
 	return a - b;
 }
 
 function multiply(a,b) {
-	
 	return a * b;
 }
 
 function divide(a,b) {
-
 	if (b == 0) {
 		return ERROR_STR_DIV; // Ausnahme (der Returnbefehl beendet die funktion, weshalb return a/ b nicht mehr ausgeführt wird.)
 	}
