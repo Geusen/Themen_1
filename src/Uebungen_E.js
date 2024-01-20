c74d86243c9b66d408df7c911cc6a7e8a6cc4e5f
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-20 17:14:38 +0100

    einbau übungsdatei für Exercism

---
+++ src/Uebungen_E.js
@@ -0,0 +1,22 @@
+let knightIsAwwake = Boolean
+let archerIsAwake = Boolean
+let prisonerIsAwake = Boolean
+if (true) {
+    knightIsAwwake = true
+    archerIsAwake = true
+    prisonerIsAwake = true
+} else {
+    
+}
+
+function canSpy(knightIsAwwake, archerIsAwake, prisonerIsAwake) {
+    
+}
+
+switch (true) {
+    case (knightIsAwwake = false, archerIsAwake = true, prisonerIsAwake = true):
+        canExecuteFastAttack = true
+
+    default:
+        canExecuteFastAttack = false
+}
